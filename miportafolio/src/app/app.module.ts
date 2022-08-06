a1d02cb62a50f71cc7e0b95e36efa380ea362181
ladypam <ladypam2424@gmail.com>
2022-08-06 16:37:10 -0300

    se crearon componentes para header , logos , redes sociales , se archivaron imagenes

--- miportafolio/src/app/app.module.ts
+++ miportafolio/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './miscomponentes/header/header.component';
+import { LogosComponent } from './miscomponentes/logos/logos.component';
+import { RedessocialesComponent } from './miscomponentes/redessociales/redessociales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogosComponent,
+    RedessocialesComponent
   ],
   imports: [
     BrowserModule,
