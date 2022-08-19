0fdeaf37bc154ee73b8160f7591fc11b9c21959b
ladypam <ladypam2424@gmail.com>
2022-08-19 15:53:30 -0300

    se agrego redes sociales a componente logo y se borro carpeta de comp.redes sociales

--- miportafolio/FRONTEND/src/app/app.module.ts
+++ miportafolio/FRONTEND/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './miscomponentes/header/header.component';
 import { LogosComponent } from './miscomponentes/logos/logos.component';
-import { RedessocialesComponent } from './miscomponentes/redessociales/redessociales.component';
 import { BanerComponent } from './miscomponentes/baner/baner.component';
 import { AcercademiComponent } from './miscomponentes/acercademi/acercademi.component';
 import { ExperienciaComponent } from './miscomponentes/experiencia/experiencia.component';
@@ -19,7 +18,6 @@ import { FooterComponent } from './miscomponentes/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogosComponent,
-    RedessocialesComponent,
     BanerComponent,
     AcercademiComponent,
     ExperienciaComponent,
